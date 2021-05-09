7e5e066c1d4a3ee383b8cdf45bf06e96f89526cc
Timi Coker <timicoker@Timis-MBP-2.lan>
2021-05-09 19:39:54 +0100

    webpack and typescript config

---
+++ frontend/client/webpack.config.ts
@@ -0,0 +1,14 @@
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js', // string
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 8080
+  }
+};
